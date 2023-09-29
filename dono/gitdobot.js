58a20de5835ff70234710d39f7b0faf915d7bcbf
ANDREKTM <146466793+ANDREKTM@users.noreply.github.com>
2023-09-29 13:43:21 -0300

    Update ⁰⁷⁵ TRALHAS

--- dono/gitdobot.js
+++ dono/gitdobot.js
@@ -1,6 +1,6 @@
 const gitdobot = (prefix) => {
 return`
-*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NOS COMENTÁRIOS FIXADOS :) https://youtube.com/@MarcioScheyot*
+*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NOS COMENTÁRIOS FIXADOS :) https://www.youtube.com/@075_andre*
 `
 }
 
